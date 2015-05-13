808a3428d7c146c539c83cf6af2044a23bcce997
Chris Miller <chris@swatman-miller.com>
2015-05-13 07:56:48 -0400

    added comments to instructions

--- repeat_instructions.sh
+++ repeat_instructions.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# This bash script, for use with Mac OSX's `say` command
+# takes two parameters. 
+# Parameters:
+#	n	Number of times to repeat the instruction
+#	s	Quoted string to repeat (usually an instruction)
+# Examples:
+# `./repeat_instructions.sh 5 "I love you, now get your teeth brushed"`
+
 instruction="$2"
 echo "Repeating instruction $1 times"; 
 echo "Instruction $instruction";
